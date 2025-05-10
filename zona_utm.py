392d21fc4af1bb6b5cfdaa2f65f45cb18794081f
MARCIOCOSTAPI <66238992+MARCIOCOSTAPI@users.noreply.github.com>
2025-05-10 01:40:03 -0300

    Add files via upload

---
+++ zona_utm.py
@@ -0,0 +1,17 @@
+import math
+import geopandas as gpd
+
+
+def calcular_utm(gdf):
+    gdf = gdf.dissolve(by=None)
+    centroid = gdf.centroid.iloc[0]
+    zona = math.ceil((centroid.x+180)/6)
+    if centroid.y > 0:
+        epsg = zona + 326000
+    else:
+        epsg = zona + 32700
+    
+    return epsg
+
+
+                            
